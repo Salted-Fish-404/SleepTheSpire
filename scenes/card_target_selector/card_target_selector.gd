7fbc9d86407ea228840d27f3778313d34f61aafd
Yuan Liang <284451276@qq.com>
2024-05-23 13:46:03 +0800

    v1.2-feature

--- scenes/card_target_selector/card_target_selector.gd
+++ scenes/card_target_selector/card_target_selector.gd
@@ -63,13 +63,12 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 	if not current_card.targets.has(area):
 		current_card.targets.append(area)
 		
+		
 func _on_area_2d_area_exited(area: Area2D) -> void:
 	if not current_card or not targeting:
 		return
 		
 	current_card.targets.erase(area)
-
-
 
 
 
